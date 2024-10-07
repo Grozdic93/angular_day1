8b3328b148d645de06ec44337d6813e66354f785
Grozdic93 <dusgrozdic@gmail.com>
2024-10-07 17:17:19 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ButtonComponent } from './button/button.component';
+import { FooterComponent } from './footer/footer.component';
+import { Hero2Component } from './hero2/hero2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent,
+    AboutComponent,
+    ButtonComponent,
+    FooterComponent,
+    Hero2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  cardTitle1:string = 'Card 1';
+  cardTitle2:string = 'Card 2';
+  cardTitle3:string = 'Card 3';
+}
